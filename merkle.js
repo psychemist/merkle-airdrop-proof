35b7ef9c85a0ee103142f25b0b842d59248aae42
psych0wh1z <ikeagudike@gmail.com>
2024-09-11 06:12:48 +0100

    Complete merkle.js script

--- merkle.js
+++ merkle.js
@@ -1,21 +1,37 @@
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 import csv from "csv-parser";
 import fs from "fs";
+import { constants } from "fs/promises";
+import keccak256 from "keccak256";
 
-const filePath = "./addresses.csv"
+const filePath = "./addresses.csv";
+const leaves = [];
 
 fs.createReadStream(filePath)
-    .on('error', (err) => {
+    .pipe(csv())
+    .on("data", (row) => {
+        const userData = [row.address, row.amount];
+        leaves.push(userData);
+    })
+    .on("end", () => {
+        const tree = StandardMerkleTree.of(leaves, ["address", "uint256"]);
+        fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
+        console.log("Merkle Root:", tree.root);
+
+        const proofs = [];
+        const merkleTree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("tree.json", "utf8")))
+
+        for (const [i, v] of merkleTree.entries()) {
+            const proof = merkleTree.getProof(i);
+            proofs.push({ [v]: proof });
+        }
+
+        fs.writeFileSync("proofs.json", JSON.stringify(proofs), "utf-8");
+        console.log("All proofs saved successfully");
+    })
+    .on("error", (err) => {
         console.error(err);
     })
 
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log(row);
-    })
-
-    .on('end', () => {
-        // handle end of CSV
-    })
 
   // Merkle Root: 0x2901c586b45328dd13a5607de2890825bd4510e10653eaac223f8507ec6766c4
--- scripts/merkle.js
+++
@@ -1,6 +0,0 @@
-import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
-import csv from "csv-parser";
-import fs from "fs";
-
-
-  // Merkle Root: 0x2901c586b45328dd13a5607de2890825bd4510e10653eaac223f8507ec6766c4
