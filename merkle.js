e9c5198a7d94fd2e0f9cfefc5b76f566e58bff25
psych0wh1z <ikeagudike@gmail.com>
2024-09-11 06:41:22 +0100

    Add comments to merkle.js

--- merkle.js
+++ merkle.js
@@ -10,23 +10,31 @@ const leaves = [];
 fs.createReadStream(filePath)
     .pipe(csv())
     .on("data", (row) => {
+        // Store user wallet address and airdrop amount in array
         const userData = [row.address, row.amount];
         leaves.push(userData);
     })
     .on("end", () => {
         try {
+            // Build Merkle tree whose leaves are hashed userData
             const tree = StandardMerkleTree.of(leaves, ["address", "uint256"]);
+            // JSONify Merkle tree and write to file
             fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
             console.log("Merkle Root:", tree.root);
 
+            // Read stored Merkle tree from file
             const merkleTree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("tree.json", "utf8")))
             const proofs = {};
 
+            // Loop through tree entries (index, userData[])
             for (const [i, v] of merkleTree.entries()) {
+                // Generate proof for each tree leaf
                 const proof = merkleTree.getProof(i);
+                // Add generated proof to proofs object
                 proofs[v[0]] = proof;
             }
 
+            // Write proofs object to file
             fs.writeFileSync("proofs.json", JSON.stringify(proofs, null, 2), "utf8");
             console.log("Proofs saved successfully");
         } catch (err) {
