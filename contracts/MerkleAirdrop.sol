bfd33a36586c28cdf469848f410c619f9bf8f0fe
psych0wh1z <ikeagudike@gmail.com>
2024-09-18 15:27:09 +0100

    Write MerkleAirdrop contract

--- contracts/MerkleAirdrop.sol
+++ contracts/MerkleAirdrop.sol
@@ -2,43 +2,55 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdrop is Ownable {
-    bytes32 public merkleRoot;
-    IERC20 public token;
+    bytes32 merkleRoot;
+    IERC20 token;
 
-    mapping(address => bool) public validClaims;
+    mapping(address => bool) claimants;
 
-    event AirdropClaimedSuccessfuly(address indexed claimant, uint256 amount);
+    event AirdropClaimed(address indexed claimant, uint256 amount, uint32 timestamp);
 
-    constructor(address _token, bytes32 _merkleRoot) Ownable(msg.sender) {
-        token = IERC20(_token);
+    constructor(address _tokenAddress, bytes32 _merkleRoot) Ownable(msg.sender) {
+        token = IERC20(_tokenAddress);
         merkleRoot = _merkleRoot;
     }
 
-    function claimAirdrop(bytes32[] memory _proof, uint _amount) external {
-        require(!validClaims[msg.sender], "Address has claimed airdrop!");
+    function claimAirdrop(
+        address _claimant,
+        uint256 _amount,
+        bytes32[] memory _proof
+    ) external {
+        require(msg.sender != address(0), "Address Zero forbidden!")
+        require(_claimant != address(0), "Address Zero not valid claimant!")
 
-        bytes32 leaf = keccak256(
-            bytes.concat(keccak256(abi.encode(msg.sender, _amount)))
-        );
+        require(!claimants[_claimant], "Airdrop already claimed!")
+        require(_amount > 0, "Cannot claim zero tokens!")
+        require(token.balanceOf(address(this)) >= _amount, "All airdrop tokens claimed!")
 
-        require(
-            MerkleProof.verify(_proof, merkleRoot, leaf),
-            "Proof not valid!"
-        );
+        // Compute leaf hash for the provided address and amount ABI encoded values
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(_claimant, _amount))));
+        // Verify leaf hash using MerkleProof's verify function.
+        require(MerkleProof.verify(_proof, merkleRoot, leaf), "Invalid proof submmitted");
 
-        // Add address to claimants' mapping before sending tokens to prevent reentrancy
-        validClaims[msg.sender] = true;
+        // Mark claimant has received tokens
+        claimants[_claimant] = true;
 
-        IERC20(token).transfer(msg.sender, _amount);
-        emit AirdropClaimedSuccessfuly(msg.sender, _amount);
+        // Transfer airdrop tokens to claimant
+        token.transfer(_claimant, _amount);
+
+        emit AirdropClaimed(_claimant, _amount,block.timestamp);
     }
 
-    function withdrawAllTokens() external onlyOwner {
-        uint256 balance = token.balanceOf(address(this));
-        require(token.transfer(owner(), balance), "Token transfer failed1");
+    function updateMerkleRoot(bytes32 _newMerkleRoot) external onlyOwner () {
+        require(token.balanceOf(address(this)), "Airdrop tokens exhausted!");
+        merkleRoot = _newMerkleRoot;
+    }
+
+    function withdraw() external onlyOwner {
+        // Withdraw all airdrop tokens to contract owner's address
+        require(token.transfer(_owner, token.balanceOf(address(this))), "Transfer failed");
     }
 }
